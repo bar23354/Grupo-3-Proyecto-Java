fd33c291d85b6cdf5f60c25da499343858ed3ccb
jlope384 <lop23415@uvg.edu.gt>
2023-08-04 09:05:32 -0600

    a

--- WritetoFile.java
+++ WritetoFile.java
@@ -1,43 +1,25 @@
 //Lopez
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WritetoFile {
+    private String filePath;
+
+    public WritetoFile(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void writeStringToCsv(String data) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(data);
+            System.out.println("El archivo CSV se ha creado y guardado exitosamente.");
+    }
+
     public static void main(String[] args) {
-        String inputFilePath = "input.txt"; // Ruta del archivo de entrada
-        String outputFilePath = "output.txt"; // Ruta del archivo de salida
-        
-        // Leer el archivo de entrada y escribir en el archivo de salida
-        try {
-            String content = writeFile(inputFilePath);
-            writeFile(outputFilePath, content);
-            System.out.println("Contenido del archivo leído y escrito en el nuevo archivo.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    // Método para leer el contenido de un archivo
-    public static String writefile(String filePath) throws IOException {
-        StringBuilder content = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-        }
-        return content.toString();
-    }
-    
-    // Método para escribir contenido en un archivo
-    public static void writeFile(String filePath, String content) throws IOException {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
-            bw.write(content);
-        }
+        String dataToWrite = "Hola,este es un ejemplo de texto que se escribirá en el CSV.";
+        CsvWriter csvWriter = new CsvWriter("ruta/del/archivo.csv"); // Reemplaza "ruta/del/archivo.csv" por la ruta deseada
+        csvWriter.writeStringToCsv(dataToWrite);
     }
 }
-
