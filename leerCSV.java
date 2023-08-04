0cdfd7505d1a5cb76a6d1adda22adc8d581a926f
dlop6 <lop23747@uvg.edu.gt>
2023-08-04 08:36:19 -0600

    si

--- leerCSV.java
+++ leerCSV.java
@@ -7,18 +7,15 @@ import java.util.Scanner;
 
 
 public class leerCSV{
-    public static void readCSV(String filePath) {
+    public static String[] readCSV(String filePath) { // Esta función lee un archivo CSV y lo imprime en pantalla
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 // Process each line of the CSV file here
                 String[] data = line.split(",");
-                String name = data[0];
-                int age = Integer.parseInt(data[1]);
-                String occupation = data[2];
 
                 // Do something with the data (e.g., print it)
-                System.out.println("Name: " + name + ", Age: " + age + ", Occupation: " + occupation);
+                return data;
             }
         } catch (IOException e) {
             e.printStackTrace();
