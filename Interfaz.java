a6780997f3567a5f0060613e27b4867328fd3a83
bar23354 <bar23354@uvg.edu.gt>
2023-08-04 08:30:33 -0600

    Changes 23354

--- Interfaz.java
+++ Interfaz.java
@@ -1,3 +1,11 @@
+import java.util.List;
+
 public class Interfaz {
-    
+    public static void main(String[] args) {
+        List<String> palindrome = ListString.getPalindromeList();
+
+        for (String papaleta : palindrome) {
+            System.out.println(papaleta);
+        }
+    }
 }
