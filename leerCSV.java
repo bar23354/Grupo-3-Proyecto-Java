1fabd1ae88b26a4c83140b15a53dd5089c6a0c03
dlop6 <lop23747@uvg.edu.gt>
2023-08-04 08:29:28 -0600

    leerCSV 23747

--- leerCSV.java
+++ leerCSV.java
@@ -1,19 +1,30 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 
-
 public class leerCSV{
-    final LeerArchivo archivo = new LeerArchivo();
-    final Scanner entrada = new Scanner(System.in);
-    
+    public static void readCSV(String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Process each line of the CSV file here
+                String[] data = line.split(",");
+                String name = data[0];
+                int age = Integer.parseInt(data[1]);
+                String occupation = data[2];
 
-    public leerCSV(){
-        archivo.abrirArchivo("palabras.csv");
-        archivo.leerRegistros();
-        archivo.cerrarArchivo();
+                // Do something with the data (e.g., print it)
+                System.out.println("Name: " + name + ", Age: " + age + ", Occupation: " + occupation);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
 
 
